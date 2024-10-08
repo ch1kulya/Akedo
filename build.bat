e90129aab40080497881a6af5047ff4112efdd2f
ch1ka <164895536+ch1kulya@users.noreply.github.com>
2024-10-08 10:14:57 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 :: Pyinstaller build script
 cd C:\Users\ch1ka\Desktop\Akedo
-nuitka --standalone --output-dir=build --windows-console-mode=disable --windows-icon-from-ico=logo.ico --include-data-dir=audio=audio game.py
+nuitka --standalone --output-dir=build --windows-console-mode=disable --noinclude-default-mode=error --deployment --nofollow-import-to=unittest --windows-icon-from-ico=logo.ico --include-data-dir=audio=audio Akedo.py
